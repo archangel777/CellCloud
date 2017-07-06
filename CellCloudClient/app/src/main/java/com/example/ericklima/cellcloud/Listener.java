c4500cfc8994ed84ede78c269c454087ce985256
archangel777 <e_rickle_ao@hotmail.com>
2017-07-06 11:15:32 -0300

    Ignoring broadcast from itself.

--- CellCloudClient/app/src/main/java/com/example/ericklima/cellcloud/Listener.java
+++ CellCloudClient/app/src/main/java/com/example/ericklima/cellcloud/Listener.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 /**
  * Created by ErickLima on 01/07/2017.
@@ -16,10 +19,12 @@ import java.net.InetAddress;
 public class Listener implements Runnable {
 
     private static final String TAG = "LISTENER";
+    private static String localHost = null;
 
     @Override
     public void run() {
         try {
+            localHost = getLocalIpAddress();
             //Keep a socket open to listen to all the UDP trafic that is destined for this port
             DatagramSocket socket = new DatagramSocket(Constants.PORT, InetAddress.getByName("0.0.0.0"));
             socket.setBroadcast(true);
@@ -32,12 +37,35 @@ public class Listener implements Runnable {
                 DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
                 socket.receive(packet);
 
-                //Packet received
-                Log.i(TAG, "Packet received from: " + packet.getAddress().getHostAddress());
-                Bitmap bitmap = BitmapFactory.decodeByteArray(recvBuf, 0, recvBuf.length);
+                if (!isFromPC(packet.getAddress())) {
+                    //Packet received
+                    Log.i(TAG, "Packet received from: " + packet.getAddress().getHostAddress());
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(recvBuf, 0, recvBuf.length);
+                }
             }
         } catch (IOException ex) {
             Log.i(TAG, "Oops!! " + ex.getMessage());
         }
+    }
+
+    private boolean isFromPC(InetAddress address) {
+        return address.getHostAddress().equals(localHost);
+    }
+
+    private String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("Socket Problem", ex.toString());
+        }
+        return null;
     }
 }
